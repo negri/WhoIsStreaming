cd8c479fc5a51282bc0ad6b3815e006dfcff995e
João Paulo Gonçalves Negri <negrijp@outlook.com>
2021-02-07 16:06:39 -0300

    feat: Excel Report

--- WhoIsStreaming/Commands/Report.cs
+++ WhoIsStreaming/Commands/Report.cs
@@ -4,7 +4,6 @@ using System.Diagnostics;
 using System.Globalization;
 using System.IO;
 using System.Linq;
-using System.Text;
 using System.Threading.Tasks;
 using CliFx;
 using CliFx.Attributes;
@@ -12,6 +11,8 @@ using CliFx.Exceptions;
 using CsvHelper;
 using CsvHelper.Configuration;
 using CsvHelper.Configuration.Attributes;
+using Negri.Twitch.Api;
+using OfficeOpenXml;
 
 namespace Negri.Twitch.Commands
 {
@@ -31,7 +32,8 @@ namespace Negri.Twitch.Commands
         [CommandOption("excel-file", 'e', Description = "The Excel report to write.")]
         public string ExcelFile { get; set; }
 
-        [CommandOption("data-dir", 'd', Description = "The directory where to read collected data.", EnvironmentVariableName = "who-is-streaming-data-dir", IsRequired = true)]
+        [CommandOption("data-dir", 'd', Description = "The directory where to read collected data.", EnvironmentVariableName = "who-is-streaming-data-dir",
+            IsRequired = true)]
         public string DataDir { get; set; } = string.Empty;
 
         [CommandOption("start", Description = "The start moment to report.")]
@@ -54,10 +56,10 @@ namespace Negri.Twitch.Commands
 
             if (!Directory.Exists(DataDir))
             {
-                throw new CommandException($"The data directory '{DataDir}' does not exists.", (int)ReturnCode.DataDirectoryDoesNotExists);
+                throw new CommandException($"The data directory '{DataDir}' does not exists.", (int) ReturnCode.DataDirectoryDoesNotExists);
             }
 
-            var client = new Api.TwitchClient(_appSettings.ClientId, _appSettings.ClientSecret);
+            var client = new TwitchClient(_appSettings.ClientId, _appSettings.ClientSecret);
             client.Logon();
 
             // Check the game
@@ -86,13 +88,13 @@ namespace Negri.Twitch.Commands
             console.Output.WriteLine($"{observations.Count:N0} observations found, building sessions...");
             if (observations.Count <= 0)
             {
-                throw new CommandException("No streams where found.", (int)ReturnCode.NoObservations);
+                throw new CommandException("No streams where found.", (int) ReturnCode.NoObservations);
             }
 
             var sessions = GetSessions(observations.OrderBy(o => o.Moment)).OrderByDescending(s => s.Observations).ToList();
             console.Output.WriteLine($"{sessions.Count:N0} sessions found for a total of {sessions.Sum(s => s.Duration.TotalHours):N0} hours.");
 
-            console.Output.WriteLine($"Streamer                       Language Avg Viewers Max Viewers Duration");
+            console.Output.WriteLine("Streamer                       Language Avg Viewers Max Viewers Duration");
             foreach (var s in sessions)
             {
                 console.Output.WriteLine($"{s.UserName,-30} {s.Language,-8} {s.AverageViewers,11:N0} {s.MaxViewers,11:N0} {s.Duration,8:c}");
@@ -100,15 +102,69 @@ namespace Negri.Twitch.Commands
 
             if (!string.IsNullOrWhiteSpace(ExcelFile))
             {
-                WriteExcel(ExcelFile, observations, sessions);
+                WriteExcel(game.Name, observations, sessions);
+                console.Output.WriteLine($"Excel report saved on '{ExcelFile}'.");
             }
 
             return default;
         }
 
-        private void WriteExcel(string file, List<Observation> observations, List<Session> sessions)
+        private void WriteExcel(string gameName, List<Observation> observations, List<Session> sessions)
         {
-           
+            var templateFile = Path.Combine(AppContext.BaseDirectory, "Template.xlsx");
+
+            var templateFileInfo = new FileInfo(templateFile);
+            using var package = new ExcelPackage(templateFileInfo);
+
+            // The sessions
+            var sessionsSheet = package.Workbook.Worksheets["Sessions"];
+            sessionsSheet.Cells[2, 1].Value = gameName;
+
+            var row = 8;
+            foreach (var session in sessions)
+            {
+                sessionsSheet.Cells[row, 1].Value = session.UserId;
+                sessionsSheet.Cells[row, 2].Value = session.UserName;
+                sessionsSheet.Cells[row, 3].Value = session.Language;
+                sessionsSheet.Cells[row, 4].Value = UseUtc ? session.Start : session.Start.ToLocalTime();
+                sessionsSheet.Cells[row, 5].Value = UseUtc ? session.End : session.End.ToLocalTime();
+                sessionsSheet.Cells[row, 6].FormulaR1C1 = "=RC[-1]-RC[-2]";
+                sessionsSheet.Cells[row, 7].Value = session.MaxViewers;
+                sessionsSheet.Cells[row, 8].Value = session.AverageViewers;
+                sessionsSheet.Cells[row, 9].Value = session.Observations;
+                sessionsSheet.Cells[row, 10].Value = session.Title;
+
+                ++row;
+            }
+
+            sessionsSheet.Cells[4, 2].FormulaR1C1 = $"=MIN(R8C4:R{row - 1}C4)";
+            sessionsSheet.Cells[5, 2].FormulaR1C1 = $"=MAX(R8C5:R{row - 1}C5)";
+
+            // The observations
+            var observationsSheet = package.Workbook.Worksheets["Observations"];
+            observationsSheet.Cells[2, 1].Value = gameName;
+
+            row = 8;
+            foreach (var observation in observations)
+            {
+                observationsSheet.Cells[row, 1].Value = observation.UserId;
+                observationsSheet.Cells[row, 2].Value = observation.UserName;
+                observationsSheet.Cells[row, 3].Value = observation.Language;
+                observationsSheet.Cells[row, 4].Value = UseUtc ? observation.Start : observation.Start.ToLocalTime();
+                observationsSheet.Cells[row, 5].Value = UseUtc ? observation.Moment : observation.Moment.ToLocalTime();
+                observationsSheet.Cells[row, 6].FormulaR1C1 = "=RC[-1]-RC[-2]";
+                observationsSheet.Cells[row, 7].Value = observation.Viewers;
+                observationsSheet.Cells[row, 8].Value = observation.Title;
+
+                ++row;
+            }
+
+            observationsSheet.Cells[4, 2].FormulaR1C1 = $"=MIN(R8C4:R{row - 1}C4)";
+            observationsSheet.Cells[5, 2].FormulaR1C1 = $"=MAX(R8C5:R{row - 1}C5)";
+
+
+            var destFile = new FileInfo(ExcelFile);
+            package.SaveAs(destFile);
         }
 
         private static IEnumerable<Session> GetSessions(IEnumerable<Observation> observations)
@@ -126,7 +182,6 @@ namespace Negri.Twitch.Commands
                         UserName = o.UserName,
                         Language = o.Language,
                         Start = o.Start
-
                     };
                     sessions.Add(k, session);
                 }
@@ -145,7 +200,7 @@ namespace Negri.Twitch.Commands
                 new CsvConfiguration(CultureInfo.InvariantCulture)
                 {
                     IgnoreBlankLines = true,
-                    BadDataFound = (e => { return; })
+                    BadDataFound = _ => { }
                 });
 
             var records = csv.GetRecords<Observation>().Where(o => o.Moment > start && o.Moment <= end).ToList();
@@ -185,54 +240,33 @@ namespace Negri.Twitch.Commands
             if (Start == null && End == null && Period != null)
             {
                 End = DateTime.UtcNow;
-                switch (Period)
+                Start = Period switch
                 {
-                    case ReportPeriod.Day:
-                        Start = End.Value.AddDays(-1);
-                        break;
-                    case ReportPeriod.Week:
-                        Start = End.Value.AddDays(-7);
-                        break;
-                    case ReportPeriod.Month:
-                        Start = End.Value.AddMonths(-1);
-                        break;
-                    default:
-                        throw new ArgumentOutOfRangeException(nameof(Period), Period, "Invalid Period!");
-                }
+                    ReportPeriod.Day => End.Value.AddDays(-1),
+                    ReportPeriod.Week => End.Value.AddDays(-7),
+                    ReportPeriod.Month => End.Value.AddMonths(-1),
+                    _ => throw new ArgumentOutOfRangeException(nameof(Period), Period, "Invalid Period!")
+                };
             }
             else if (Start != null && End == null && Period != null)
             {
-                switch (Period)
+                End = Period switch
                 {
-                    case ReportPeriod.Day:
-                        End = Start.Value.AddDays(1);
-                        break;
-                    case ReportPeriod.Week:
-                        End = Start.Value.AddDays(7);
-                        break;
-                    case ReportPeriod.Month:
-                        End = Start.Value.AddMonths(1);
-                        break;
-                    default:
-                        throw new ArgumentOutOfRangeException(nameof(Period), Period, "Invalid Period!");
-                }
+                    ReportPeriod.Day => Start.Value.AddDays(1),
+                    ReportPeriod.Week => Start.Value.AddDays(7),
+                    ReportPeriod.Month => Start.Value.AddMonths(1),
+                    _ => throw new ArgumentOutOfRangeException(nameof(Period), Period, "Invalid Period!")
+                };
             }
             else if (Start == null && End != null && Period != null)
             {
-                switch (Period)
+                Start = Period switch
                 {
-                    case ReportPeriod.Day:
-                        Start = End.Value.AddDays(-1);
-                        break;
-                    case ReportPeriod.Week:
-                        Start = End.Value.AddDays(-7);
-                        break;
-                    case ReportPeriod.Month:
-                        Start = End.Value.AddMonths(-1);
-                        break;
-                    default:
-                        throw new ArgumentOutOfRangeException(nameof(Period), Period, "Invalid Period!");
-                }
+                    ReportPeriod.Day => End.Value.AddDays(-1),
+                    ReportPeriod.Week => End.Value.AddDays(-7),
+                    ReportPeriod.Month => End.Value.AddMonths(-1),
+                    _ => throw new ArgumentOutOfRangeException(nameof(Period), Period, "Invalid Period!")
+                };
             }
             else if (Start != null && End == null && Period == null)
             {
@@ -246,19 +280,22 @@ namespace Negri.Twitch.Commands
 
         private class Session
         {
+            private readonly Dictionary<string, int> _titles = new(2);
+            private int _cumulativeViewers;
+
             public long UserId { get; init; }
             public string UserName { get; init; }
             public string Language { get; init; }
             public DateTime Start { get; init; }
 
             public string Title => _titles.OrderByDescending(kv => kv.Value).FirstOrDefault().Key;
-            private readonly Dictionary<string, int> _titles = new Dictionary<string, int>(2);
 
-            public DateTime End { get; set; }
-            public int Observations { get; set; }
-            public int AverageViewers => CumulativeViewers / Observations;
-            public int MaxViewers { get; set; }
-            public int CumulativeViewers { get; private set; }
+            public DateTime End { get; private set; }
+            public int Observations { get; private set; }
+            public int AverageViewers => _cumulativeViewers / Observations;
+            public int MaxViewers { get; private set; }
+
+            
 
             public TimeSpan Duration => End - Start;
 
@@ -281,36 +318,28 @@ namespace Negri.Twitch.Commands
                     MaxViewers = o.Viewers;
                 }
 
-                CumulativeViewers += o.Viewers;
+                _cumulativeViewers += o.Viewers;
             }
         }
 
         private record Observation
         {
-            [Name("User Id")]
-            public long UserId { get; init; }
+            [Name("User Id")] public long UserId { get; init; }
 
-            [Name("User Name")]
-            public string UserName { get; init; } 
+            [Name("User Name")] public string UserName { get; init; }
 
-            public string Language { get; init; } 
+            public string Language { get; init; }
 
             public int Viewers { get; init; }
 
-            [Name("Started At")]
-            public DateTime Start { get; init; }
+            [Name("Started At")] public DateTime Start { get; init; }
 
-            [Name("Running Minutes")]
-            public int Minutes { get; init; }
+            [Name("Running Minutes")] public int Minutes { get; init; }
+
 
             public string Title { get; init; }
 
             public DateTime Moment => Start.AddMinutes(Minutes);
         }
-            
-            
     }
-
-    
-    
 }
